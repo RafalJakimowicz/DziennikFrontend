7f1d59c40667d0041905e905304d32f938eb2848
Rafał Jakimowicz <bobix7913@gmail.com>
2025-05-12 23:15:52 +0200

    main

--- src/app/components/page/presence-table/presence-table.component.ts
+++ src/app/components/page/presence-table/presence-table.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {Group, GroupApiService} from '../../../services/group/group-api.service';
+import {Student} from '../../../services/student/student-api.service';
+import {Lesson, LessonApiService} from '../../../services/lesson/lesson-api.service';
+import {Attendance, AttendanceApiService} from '../../../services/attendance/attendance-api.service';
+import {WebTokenService} from '../../../services/token/web-token.service';
 
 @Component({
   selector: 'app-presence-table',
@@ -9,22 +14,88 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './presence-table.component.html',
   styleUrls: ['./presence-table.component.css']
 })
-export class PresenceTableComponent {
-  groups: string[] = ['1A', '1B', '2A'];
-  selectedGroup: string = this.groups[0];
-  selectedDate: string = new Date().toISOString().split('T')[0];
+export class PresenceTableComponent implements OnInit {
+  constructor(private groupsService: GroupApiService, private lessonService: LessonApiService, private token: WebTokenService, private attendanceService: AttendanceApiService) {
+  }
 
-  students = [
-    { name: 'Jan Kowalski', status: 'obecny' },
-    { name: 'Anna Nowak', status: 'obecny' },
-    { name: 'Piotr Wiśniewski', status: 'obecny' }
-  ];
+  groups: Group[] = [];
+  students: Student[] = [];
+  selectedGroup: Group = this.groups[0];
+  attendanceDate: string = new Date().toISOString().split('.')[0];
+  selectedDate: string = this.attendanceDate.split('T')[0];
+  lessonSubject: string = "";
+  lessonRoom: string = "";
+
 
   submitAttendance() {
+    var classesWindow: string[] = this.lessonService.getTeachingBlock()
+    var lesson: Lesson = {
+      id: NaN,
+      startTime: classesWindow[0],
+      endTime: classesWindow[1],
+      groupId: this.selectedGroup.id,
+      room: this.lessonRoom,
+      subject: this.lessonSubject
+    }
+
+    this.lessonService.addLesson(lesson).subscribe(
+      {
+        next: lesson => {
+          for(const student of this.students) {
+            var attendance: Attendance = {
+              id: NaN,
+              lessonId: lesson.id,
+              date: classesWindow[0],
+              status: student.status,
+              studentId: student.id,
+              userId: this.token.getUserId()
+            };
+            this.attendanceService.addAttendance(attendance).subscribe(
+              {
+                next: attendance => {},
+                error: err => {console.log(err)}
+              }
+            );
+          }
+        },
+        error: err => {console.log(err)}
+      }
+    );
+
+
     console.log('Zatwierdzono obecność:', {
       group: this.selectedGroup,
-      date: this.selectedDate,
+      date: this.attendanceDate,
       students: this.students
     });
+  }
+
+  onOptionChanged($event: Event) {
+    console.log(this.selectedGroup);
+    this.groupsService.getStudentsInGroup(this.selectedGroup.id).subscribe(
+      {
+        next: result => {
+          this.students = result;
+        },
+        error: error => {
+          console.log(error);
+        }
+      }
+    )
+  }
+
+  ngOnInit() {
+    this.groupsService.getUserGroups().subscribe(
+      {
+        next: groups => {
+          this.groups = groups;
+          this.selectedGroup = groups[0];
+        },
+        error: error => {
+          console.log(error);
+          this.groups = [];
+        }
+      }
+    );
   }
 }
--- src/app/services/group/group-api.service.ts
+++ src/app/services/group/group-api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {WebTokenService} from '../token/web-token.service';
 import {Observable} from 'rxjs';
+import { Student} from '../student/student-api.service';
 
 export interface Group {
   id: number;
@@ -26,9 +27,15 @@ export class GroupApiService {
   }
 
   public getUserGroups(): Observable<Group[]> {
-    this.header["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkN2I0ZjUwNS1jMzQ2LTQ1ZjgtYmU4OC1kNWZhM2EwYWExNjkiLCJzdWIiOiJqayIsImlhdCI6MTc0NzA2MTY4MywiZXhwIjoxNzQ3MTQ4MDgzfQ.WqUHjX--UY_sih-8CkmKmCZwiEYvQ6PhcuuR5vZmdTI";
+    this.header["Authorization"] = `Bearer ${this.tokenService.getToken()?.token}`;
     //const getUrl: string = `${this.baseUrl}/my/${this.tokenService.getUserId()}`;
     const getUrl: string = `${this.baseUrl}/my/${152}`;
     return this.http.get<Group[]>(getUrl, { headers: this.header });
+  }
+
+  public getStudentsInGroup(groupId: number): Observable<Student[]>{
+    this.header["Authorization"] = `Bearer ${this.tokenService.getToken()?.token}`;
+    const getUrl: string = `${this.baseUrl}/${groupId}/students`;
+    return this.http.get<Student[]>(getUrl, { headers: this.header });
   }
 }
